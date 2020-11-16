b09365c4d5df3767d90ef2a4cfa2814b42c2dff4
crystal-csete <crystal-csete@lambdastudents.com>
2020-11-16 16:35:14 -0700

    added

--- pintereach/src/App.js
+++ pintereach/src/App.js
@@ -2,6 +2,7 @@
 import { Button } from "@material-ui/core";
 import "./App.css";
 import React, { createContext } from "react";
+import Card from './Components/Card';
 
 function App() {
   const UserContext = createContext(null);
@@ -9,6 +10,7 @@ function App() {
   return (
     <UserContext.Provider>
       <div className="App">
+        <Card />
         <Button color="primary">Hello World</Button>
       </div>
     </UserContext.Provider>
