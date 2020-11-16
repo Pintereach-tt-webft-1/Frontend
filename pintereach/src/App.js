b0f9d5ddfa4e2f8842d6bbb810bb7a09d0807505
crystal-csete <crystal-csete@lambdastudents.com>
2020-11-16 15:36:15 -0700

    added provider

--- pintereach/src/App.js
+++ pintereach/src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
-import { Button } from '@material-ui/core';
-import './App.css';
+
+import { Button } from "@material-ui/core";
+import "./App.css";
+import React, { createContext } from "react";
 
 function App() {
+  const UserContext = createContext(null);
+
   return (
-    <div className="App">
-      <Button color="primary">Hello World</Button>
-    </div>
+    <UserContext.Provider>
+      <div className="App">
+        <Button color="primary">Hello World</Button>
+      </div>
+    </UserContext.Provider>
   );
 }
 
 export default App;
+
+
