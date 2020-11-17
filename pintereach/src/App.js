e4aea84a35624eb0ee14c832fa8b4bf59ee86dd8
Robert-Rodriguez <robertmasters@hotmail.com>
2020-11-17 01:12:43 -0800

    finished making article component and adding it to the App.js file.

--- pintereach/src/App.js
+++ pintereach/src/App.js
@@ -4,6 +4,26 @@ import { Button } from "@material-ui/core";
 import "./App.css";
 import React, { createContext } from "react";
 import Card from './Components/Card';
+import ArticleList from "./Components/ArticleList";
+
+const articles = 
+  {
+      "userid": 4,
+      "username": "admin",
+      "primaryemail": "admin@lambdaschool.local",
+      "userarticles": [
+          {
+              "userarticleid": 5,
+              "articletitle": "Gaming the World: How Sports Are Reshaping Global Politics and Culture",
+              "category": "sports"
+          },
+          {
+              "userarticleid": 6,
+              "articletitle": "Robert is great",
+              "category": "misc"
+          }
+      ]
+  }
 
 
 function App() {
@@ -13,7 +33,9 @@ function App() {
 
     <UserContext.Provider>
       <div className="App">
+        <ArticleList articles={articles}/>
         <Card />
+        
         <Button color="primary">Hello World</Button>
       </div>
     </UserContext.Provider>
--- pintereach/src/Components/ArticleList.js
+++ pintereach/src/Components/ArticleList.js
@@ -1,150 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom"; //uncomment after adding link to individual cards
 
-const articles = [
-  {
-      "userid": 4,
-      "username": "admin",
-      "primaryemail": "admin@lambdaschool.local",
-      "userarticles": [
-          {
-              "userarticleid": 5,
-              "articletitle": "Gaming the World: How Sports Are Reshaping Global Politics and Culture",
-              "category": "sports"
-          },
-          {
-              "userarticleid": 6,
-              "articletitle": "Article",
-              "category": "misc"
-          }
-      ],
-      "roles": [
-          {
-              "role": {
-                  "roleid": 1,
-                  "name": "ADMIN"
-              }
-          },
-          {
-              "role": {
-                  "roleid": 2,
-                  "name": "USER"
-              }
-          },
-          {
-              "role": {
-                  "roleid": 3,
-                  "name": "DATA"
-              }
-          }
-      ]
-  }
-]
 
-function ArticleList() { //will pass props when api is set up perhaps something like ArticleList({object}) or ArticleList(props)
-  console.log('ArticleList articles: ', articles)
 
+function ArticleList(articles) { //will pass props when api is set up perhaps something like ArticleList({object}) or ArticleList(props)
+  console.log('ArticleList articles.articles.userarticles: ', articles.articles.userarticles)
+
+    //uncomment and insert inplace of <h2>{article.articletitle}</h2>after .map function, after api has been set up
     // <Link key={article.id} to={`/articles/${article.id}`}>
     // <h2>{article.articletitle}</h2>
     // </Link>
   return (
     <div className = "article-list">
-      {
-        articles.userarticles.map(article => (
-          
-            <h2>{article.articletitle}</h2>
-          
-        ))
-      }
+        <h1>ArticleList</h1>
+        {
+            articles.articles.userarticles.map(article => (
+              
+                <h3>{article.articletitle}</h3>
+              
+            ))
+          }
+        
     </div>
   );
 }
 
 export default ArticleList;
 
-
-  // ,
-  // {
-  //     "userid": 7,
-  //     "username": "cinnamon",
-  //     "primaryemail": "cinnamon@lambdaschool.local",
-  //     "userarticles": [
-  //         {
-  //             "userarticleid": 8,
-  //             "articletitle": "Article",
-  //             "category": "misc"
-  //         },
-  //         {
-  //             "userarticleid": 9,
-  //             "articletitle": "Article",
-  //             "category": "misc"
-  //         },
-  //         {
-  //             "userarticleid": 10,
-  //             "articletitle": "Article",
-  //             "category": "misc"
-  //         }
-  //     ],
-  //     "roles": [
-  //         {
-  //             "role": {
-  //                 "roleid": 2,
-  //                 "name": "USER"
-  //             }
-  //         },
-  //         {
-  //             "role": {
-  //                 "roleid": 3,
-  //                 "name": "DATA"
-  //             }
-  //         }
-  //     ]
-  // },
-  // {
-  //     "userid": 11,
-  //     "username": "barnbarn",
-  //     "primaryemail": "barnbarn@lambdaschool.local",
-  //     "userarticles": [
-  //         {
-  //             "userarticleid": 12,
-  //             "articletitle": "Article",
-  //             "category": "misc"
-  //         }
-  //     ],
-  //     "roles": [
-  //         {
-  //             "role": {
-  //                 "roleid": 2,
-  //                 "name": "USER"
-  //             }
-  //         }
-  //     ]
-  // },
-  // {
-  //     "userid": 13,
-  //     "username": "puttat",
-  //     "primaryemail": "puttat@school.lambda",
-  //     "userarticles": [],
-  //     "roles": [
-  //         {
-  //             "role": {
-  //                 "roleid": 2,
-  //                 "name": "USER"
-  //             }
-  //         }
-  //     ]
-  // },
-  // {
-  //     "userid": 14,
-  //     "username": "misskitty",
-  //     "primaryemail": "misskitty@school.lambda",
-  //     "userarticles": [],
-  //     "roles": [
-  //         {
-  //             "role": {
-  //                 "roleid": 2,
-  //                 "name": "USER"
-  //             }
-  //         }
-  //     ]
-  // }
