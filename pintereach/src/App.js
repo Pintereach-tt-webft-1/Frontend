600aa2a0483d0a3a79ca6cbf21d7be7b64ff7e63
Joshua Samaniego- Evans <joshuasamaniego-evans@Joshuas-MacBook-Pro.local>
2020-11-17 17:24:16 -0700

    styles added to navbar

--- pintereach/src/App.js
+++ pintereach/src/App.js
@@ -1,40 +1,8 @@
 import "./App.css";
-
-import React, { createContext, useState } from "react";
-import Card from './Components/Card';
-import ArticleList from "./Components/ArticleList";
-import SignIn from './Components/SignIn';
+import React, { createContext } from "react";
 import NavBar from './Components/NavBar';
-import LogIn from './Components/LogIn';
-
-
-const articles = 
-  {
-      "userid": 4,
-      "username": "admin",
-      "primaryemail": "admin@lambdaschool.local",
-      "userarticles": [
-          {
-              "userarticleid": 5,
-              "articletitle": "Gaming the World: How Sports Are Reshaping Global Politics and Culture",
-              "category": "sports"
-          },
-          {
-              "userarticleid": 6,
-              "articletitle": "Robert is great",
-              "category": "misc"
-          }
-      ]
-  }
-
 
 function App() {
-  const [savedList, setSavedList] = useState([]);
-  
-  //function that handles creating a new array with the article objects, **need to handle this using context
-  const addToSavedList = article => {
-    setSavedList([...savedList, article]);
-  };
 
   const UserContext = createContext(null);
 
@@ -45,12 +13,6 @@ function App() {
         <header> 
           <NavBar />
         </header>
-
-        <ArticleList addToSavedList={addToSavedList} articles={articles}/>
-        <Card />
-        <SignIn/>
-        <LogIn/>
- 
       </div>
     </UserContext.Provider>
   );
--- pintereach/src/Components/NavBar.js
+++ pintereach/src/Components/NavBar.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Link, Switch, Route } from 'react-router-dom'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import LogIn from './LogIn'
 import SignIn from './SignIn'
 
+const kf = keyframes`
+  100% {
+    opacity: 1;
+    transform: scale(1)
+  }
+`;
+
 const NavBarStyles = styled.nav`
+    opacity: 0;
+    animation: ${kf} 2s ease-in-out forwards;
     display: flex;
     flex-wrap: wrap;
     align-items: center;
@@ -12,13 +21,28 @@ const NavBarStyles = styled.nav`
     margin: 2%;
 `
 
-const NavLinkStyles = styled.div`
+const AllLinksStyles = styled.div`
     width: 40%;
     display: flex;
     flex-wrap: wrap;
     justify-content: space-around;
-    font-size: 20px;
+    font-size: 1.8rem;
+
+`
+
+const EachLinkStyles = styled(Link)`
     text-decoration: none;
+    color: black ;
+    padding: 1% 3%;
+    border-radius: 15px;
+    border: solid 2px black;
+    
+    &:hover {
+        transform: scale(1.15);
+        transition: transform 0.5s ease-in-out;
+        background-color: #ffffff;
+    }
+    transition: transform 0.5s ease-in-out;
 
 `
 
@@ -28,12 +52,12 @@ function NavBar() {
     return (
         <div className='NavBar'>
             <NavBarStyles>
-                <img src="https://i.imgur.com/hbzvVQU.png" width="300px" alt="pintereach logo"/>
-                <NavLinkStyles className='nav-links'>
-                    <Link to='/'>Home</Link>
-                    <Link to='/signin'>Sign In</Link>
-                    <Link to='/login'>Log In</Link>
-                </NavLinkStyles>
+                <img src="https://i.imgur.com/hbzvVQU.png" alt="pintereach logo"/>
+                <AllLinksStyles className='nav-links'>
+                    <EachLinkStyles to='/'>Home</EachLinkStyles>
+                    <EachLinkStyles to='/signin'>Sign In</EachLinkStyles>
+                    <EachLinkStyles to='/login'>Log In</EachLinkStyles>
+                </AllLinksStyles>
             </NavBarStyles>
             <Switch>
                 <Route path='/signin'>
